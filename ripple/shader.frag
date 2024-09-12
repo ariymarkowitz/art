d669d3013d26e183206c6215fec89b182edb3037
Ari Markowitz <ariymarkowitz@gmail.com>
2024-09-12 15:45:58 +1200

    Rename things

---
+++ ripple/shader.frag
@@ -0,0 +1,73 @@
+#define TAU 6.28318530718
+
+uniform float time;
+uniform vec2 viewportSize;
+uniform float high;
+uniform float low;
+uniform float segments;
+
+float root(float b, float c, float d, float n) {
+    // t^3 + b*t^2 + c*t + d = 0.
+    // Always three real roots. Take the middle one.
+    float p = c - b*b/3.0;
+    float q = b*(2.0*b*b - 9.0*c)/27.0 + d;
+    
+    float t = 2.0*sqrt(-p/3.0);
+    float phi = (acos(3.0*q/(p*t)) - n*TAU)/3.0;
+
+    return t*cos(phi) - b/3.0;
+}
+
+float gradient(float t, float x, float y, float a, float b) {
+    // dt/dx = (2 t x)/(2 a b-4 a t-4 b t+3 t^2-x^2+2 a y+2 b y-y^2)
+    float dt_dx = 2.0*t*x / (2.0*a*b - 4.0*a*t - 4.0*b*t + 3.0*t*t - x*x + 2.0*a*y + 2.0*b*y - y*y);
+
+    // dt/dy = (2 (a b-a t-b t+t y))/(2 a b-4 a t-4 b t+3 t^2-x^2+2 a y+2 b y-y^2)
+    float dt_dy = 2.0*(a*b - a*t - b*t + t*y) / (2.0*a*b - 4.0*a*t - 4.0*b*t + 3.0*t*t - x*x + 2.0*a*y + 2.0*b*y - y*y);
+
+    return sqrt(dt_dx*dt_dx + dt_dy*dt_dy);
+}
+
+float stripe(float t) {
+    return mod(floor(t*segments - time/2.0), 2.0);
+}
+
+vec3 color(float t, float top, float bottom) {
+    // Normalize to [0, 1].
+    float s = (t - top) / (bottom - top);
+
+    float rad = abs(bottom+top-bottom*top/t)/viewportSize.y;
+    // Larger radius is darker. More segments is darker.
+    float dist = pow(tanh(min(rad,10.0)),2.0)*clamp(sqrt((segments-10.0)/200.0), 0.0, 1.0);
+
+    float stripe = stripe(s);
+    vec3 shade = mix(vec3(1.0), vec3(0.7), dist);
+
+    // Higher values are more gold, lower values are more purple.
+    // We saturate the colors when the source/sink is far from the middle.
+    vec3 purple = mix(vec3(0.7, 0.7, 0.9), vec3(0.8, 0.8, 0.9), -low);
+    vec3 gold = mix(vec3(0.8, 0.8, 0.7), vec3(0.9, 0.9, 0.8), high);
+    vec3 color = mix(vec3(1.0), mix(purple, gold, s), dist);
+
+    return vec3(stripe*shade*color);
+}
+
+void main() {
+    float x = gl_PointCoord.x * viewportSize.x;
+    float y = -gl_PointCoord.y * viewportSize.y;
+    float bottom = low*viewportSize.y;
+    float top = high*viewportSize.y;
+
+    // t^3 + b*t^2 + c*t + d = 0.
+    float b = -2.0*(top + bottom);
+    float c = 2.0*(top + y)*(bottom + y) - x*x - 3.0*y*y;
+    float d = -2.0*top*bottom*y;
+
+    float t = root(b, c, d, time/16.0);
+
+    // Anti-aliasing
+    float grad = gradient(t, x, y, top, bottom);
+    vec3 col = color(t, top, bottom);
+
+    gl_FragColor = vec4(col, 1.0);
+}
--- voidpool/voidpool.js
+++ voidpool/voidpool.js
@@ -1,20 +1,4 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(100);
-}
-
-function circ(y, low, high) {
-	const y2 = y+2*(low-y) + (low-y)*2*high/y;
-	if (!Number.isFinite(y2)) {
-		line(-width, 0, width, 0);
-	}
-	circle(0, (y + y2)/2, (y2-y)/2);
-}
-
-function clerp(a, b, t) {
-    t = Math.max(0, Math.min(1, t));
-    return lerp(a, b, t);
-}
+import * as THREE from 'three'
 
 function ease(a, b, delta) {
     if (a == null) {
@@ -25,39 +9,73 @@ function ease(a, b, delta) {
     return a*totalDecay + b*(1-totalDecay);
 }
 
-let n = 0;
-
-let low = null;
-let high = null;
-let spacing = null;
-
-function draw() {
-	ellipseMode(RADIUS);
-	
-    spacing = ease(spacing, Math.max(1, (width-mouseX)/100), deltaTime);
-    low = ease(low, clerp(0.9*height/2, 10, mouseY/height), deltaTime);
-    high = ease(high, clerp(10, 0.9*height/2, mouseY/height), deltaTime);
-	const s = -1/(60*1000/126)/2;
-	
-	n += s*deltaTime;
-    let n2 = n*spacing + low;
-
-    n2 = n2 % (2*spacing);
-    const startId = Math.ceil((-high - n2)/spacing);
-    const endId = Math.floor((low - n2)/spacing);
-	
-    const zeroId = Math.floor((-n2)/spacing);
-    background(zeroId % 2 ? 0 : 255);
-
-    noStroke();
-
-	translate(width/2, height/2);
-	for (let i=zeroId; i >= startId; i--) {
-        fill(i % 2 ? 255 : 0);
-		circ(i*spacing+n2, low, high);
-	}
-    for (let i=zeroId+1; i <= endId; i++) {
-        fill(i % 2 ? 0 : 255);
-		circ(i*spacing+n2, low, high);
-	}
+function clerp(a, b, t) {
+    const lerp = t*b + (1-t)*a
+    const min = Math.min(a, b)
+    const max = Math.max(a, b)
+    return Math.min(max, Math.max(min, lerp))
 }
+
+const vertexShader = await fetch('shader.vert').then(res => res.text())
+const fragmentShader = await fetch('shader.frag').then(res => res.text())
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
+const geometry = new THREE.PlaneGeometry(2, 2)
+
+let width, height
+
+let high = 0.8;
+let low = -0.1;
+let segments = 10;
+
+const material = new THREE.ShaderMaterial( {
+    uniforms: {
+        time: { value: 0 },
+        viewportSize: { value: new THREE.Vector2() },
+        high: { value: high },
+        low: { value: low },
+        segments: { value: segments }
+    },
+    vertexShader, fragmentShader
+} )
+
+const plane = new THREE.Mesh(geometry, material)
+scene.add(plane)
+
+let prevTime = null;
+
+function animate(time) {
+    material.uniforms.time.value = time / 1000 * (126/60)
+    material.uniforms.viewportSize.value.set(width, height)
+
+    const deltaTime = prevTime != null ? time - prevTime : 0
+
+    material.uniforms.high.value = ease(material.uniforms.high.value, high, deltaTime)
+    material.uniforms.low.value = ease(material.uniforms.low.value, low, deltaTime)
+    material.uniforms.segments.value = ease(material.uniforms.segments.value, segments, deltaTime)
+
+    renderer.render(scene, camera)
+    prevTime = time
+    requestAnimationFrame(animate)
+}
+
+const renderer = new THREE.WebGLRenderer()
+function setBounds() {
+    width = window.innerWidth * window.devicePixelRatio
+    height = window.innerHeight * window.devicePixelRatio
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+renderer.setPixelRatio(window.devicePixelRatio)
+setBounds()
+window.addEventListener('resize', setBounds)
+window.addEventListener('mousemove', (e) => {
+    segments = clerp(10, window.innerWidth/4, e.x / window.innerWidth)
+    high = clerp(0.9, 0.02, e.y / window.innerHeight)
+    low = clerp(-0.02, -0.9, e.y / window.innerHeight)
+})
+
+document.body.appendChild(renderer.domElement)
+
+requestAnimationFrame(animate)
